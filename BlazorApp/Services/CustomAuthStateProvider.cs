73f67bdb45bfe558af308c2138f41548c5f3f6ef
Gift <direodirile95@gmail.com>
2025-01-24 12:29:23 +0200

    created a new blazor project which is our front end. i am half way adding authentication and authorization on app.razor

---
+++ BlazorApp/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorApp.Services
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity());
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+    }
+}

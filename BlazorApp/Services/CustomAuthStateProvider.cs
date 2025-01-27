4629183057fcb57e379005cdd1ae6f13a32fae2b
Gift <direodirile95@gmail.com>
2025-01-27 11:02:20 +0200

    adding authentication and authorization ,did that on the customauthstateprovider and also worked on the layout and navgationmenu of the client side.Also added this 3 services in the client side of the app .builder.Services.AddCascadingAuthenticationState(); builder.Services.AddAuthorizationCore(); builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();

--- BlazorApp/Services/CustomAuthStateProvider.cs
+++ BlazorApp/Services/CustomAuthStateProvider.cs
@@ -7,7 +7,11 @@ namespace BlazorApp.Services
     {
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var user = new ClaimsPrincipal(new ClaimsIdentity());
+           // var user = new ClaimsPrincipal(new ClaimsIdentity());// non-autheticated
+
+           var claims = new List<Claim> { new Claim(ClaimTypes.Name, "John") };
+           var identity = new ClaimsIdentity(claims, "Any");
+            var user = new ClaimsPrincipal(identity);
 
             return Task.FromResult(new AuthenticationState(user));
         }

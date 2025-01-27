85728780f8b670abecc96168f4aa801176cc6c10
Gift <direodirile95@gmail.com>
2025-01-27 11:56:54 +0200

    worked on the httpclient side of the project basically sending in login requests and giving authentication based on their responses

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -8,7 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["webApiAdress"]!) });//this where we lauched the establishment between the backend and the client side
+//connecting to the web api 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
--- BlazorApp/Services/CustomAuthStateProvider.cs
+++ BlazorApp/Services/CustomAuthStateProvider.cs
@@ -1,19 +1,101 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Json;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Security;
 using System.Security.Claims;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
 namespace BlazorApp.Services
 {
     public class CustomAuthStateProvider : AuthenticationStateProvider
     {
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        private readonly HttpClient httpClient;
+
+        public CustomAuthStateProvider(HttpClient httpClient) {
+            this.httpClient = httpClient;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-           // var user = new ClaimsPrincipal(new ClaimsIdentity());// non-autheticated
+            // var user = new ClaimsPrincipal(new ClaimsIdentity());// non-autheticated
 
-           var claims = new List<Claim> { new Claim(ClaimTypes.Name, "John") };
-           var identity = new ClaimsIdentity(claims, "Any");
-            var user = new ClaimsPrincipal(identity);
+            //var claims = new List<Claim> { new Claim(ClaimTypes.Name, "John") };
+            //var identity = new ClaimsIdentity(claims, "Any");
+            //var user = new ClaimsPrincipal(identity);
 
-            return Task.FromResult(new AuthenticationState(user));
+            // return Task.FromResult(new AuthenticationState(user));
+            var user = new ClaimsPrincipal(new ClaimsIdentity());
+
+            try
+            {
+                var response = await httpClient.GetAsync("manage/info");
+                if (response.IsSuccessStatusCode)//if we get a successful reponse
+                {
+                    var strResponse = await response.Content.ReadAsStringAsync();//we read the response as a string
+                    var jsonResponse = JsonNode.Parse(strResponse);//then we will convert the stting to a jsoon response
+                    var email = jsonResponse!["email"]!.ToString();//read the email address from the user from the json reponse 
+
+                    var claims = new List<Claim>
+                    {
+                        new(ClaimTypes.Name, email),
+                        new(ClaimTypes.Email, email),
+
+                    };
+
+                    var identity = new ClaimsIdentity(claims , "Token");//claims iidentity
+                    user = new ClaimsPrincipal(identity);
+
+                    return new AuthenticationState(user);
+                }
+
+
+            }
+            catch (Exception ex) {
+            
+            }
+
+            return new AuthenticationState(user);
+        }
+
+        public async Task<Formresult> LoginAsync(String email, string password)
+        {
+            try
+            {
+                var response = await httpClient.PostAsJsonAsync("login", new { email, password });
+                if (response.IsSuccessStatusCode)//if we have a success response.
+                {
+                    var strResponse = await response.Content.ReadAsStringAsync();//we read the response as a string
+                    var jsonResponse = JsonNode.Parse(strResponse);///then convert the response in a json format
+                    var accessToken = jsonResponse?["accessToken"]?.ToString();//then read the access token as well the refresh token down belowe
+                    var refreshToken = jsonResponse?["refreshToken"]?.ToString();
+
+                    //then we added the access token of the authorization header of the httpclient
+                    httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);//bearer is added with capital b and the access token
+
+                    NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());//refresh auth state,then we basically updating the authentication state of the user,then we call a method getauthenticatedstateAsyn())
+
+                    return new Formresult { Succeeded = true };
+
+
+
+                }
+                else
+                {
+                    return new Formresult { Succeeded = false, Errors = ["incorrect email and password"] };
+                }
+            }
+            catch { }
+
+            return new Formresult { Succeeded = false, Errors = ["connection Error"] };
         }
     }
+    public class Formresult
+    {
+        public bool Succeeded { get; set; }
+
+        public string[] Errors { get; set; } = [];
+
+        }
+    
 }
